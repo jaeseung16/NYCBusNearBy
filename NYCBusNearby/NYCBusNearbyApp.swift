c51303bc90c71d6803870e3c2ff955e831927f97
Jae-Seung Lee <jaeseung@gmail.com>
2023-01-13 17:50:23 -0500

    Initial Commit

---
+++ NYCBusNearby/NYCBusNearbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCBusNearbyApp.swift
+//  NYCBusNearby
+//
+//  Created by Jae Seung Lee on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct NYCBusNearbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
